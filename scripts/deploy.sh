f47002f8e983503338da5deda24cc32de0937afc
4maggio <github@herdmanns.de>
2026-04-02 04:22:01 +0200

    fix: revert to plain docker exec (deploy runs as root)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,7 +24,7 @@ else
 fi
 
 echo "==> Reloading nginx config (zero-downtime)"
-sudo /usr/bin/docker exec traefik-uebernahme-1 nginx -s reload
+docker exec traefik-uebernahme-1 nginx -s reload
 
 echo "==> Restarting application with PM2"
 cd "$VPS_PATH"
